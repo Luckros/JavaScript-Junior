0932eb97de51853d7471c1298192be7d835ed1d3
Lucas Rosselli <lucasarosselli@gmail.com>
2026-03-03 20:00:30 -0300

    Cofla 5

---
+++ Capítulo 5/Problema A/codigo.js
@@ -0,0 +1,11 @@
+let materias = {
+    fisica: [90,6,4],
+    matematica: [84,8,8],
+    logica: [92,8,4],
+    quimica: [96,8,4],
+    calculo: [91,6,3],
+    programacion: [79,7,4],
+    biologia: [75,9,2],
+    bbdd: [98,9,1],
+    algebra: [100,10,4],   
+} 
