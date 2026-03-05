488ce589206dc1f058d9dead1a2671520a56446a
Lucas Rosselli <lucasarosselli@gmail.com>
2026-03-05 19:45:54 -0300

    Finalización capítulo 5

---
+++ Capítulo 5/Problema B/codigo.js
@@ -0,0 +1,31 @@
+let tp = "100 minutos hacer trabajos prácticos";
+let estudio = "100 minutos descanso";
+let trabajo = "240 minutos de trabajo";
+let homework = "30 minutos tareas del hogar";
+let descanso = "10 minutos de descanso";
+
+
+console.log("TAREAS");
+
+
+for (var i = 0; i < 14; i++) {
+    if (i==0) {
+        console.group("semana 1")
+    }
+console.groupCollapsed("dia  " + (i+1));
+console.log(trabajo);
+console.log(descanso);
+console.log(estudio);
+console.log(tp);
+console.log(homework);
+console.groupEnd();
+if (i==7) {
+    console.groupEnd();
+    console.groupCollapsed("semana 2");
+}
+}
+
+console.groupEnd();
+console.groupEnd();
+
+
